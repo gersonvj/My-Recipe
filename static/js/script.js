abeb011fedc2b6ed98f85326a0fd90a9b2b3f801
gersonvj <gersonvj@hotmail.com>
2021-01-28 21:45:49 +0000

    adding materialize carousel component image slider

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+        fullWidth: true,
+        padding: 20,
+        shift: 5,
+
+    });
+  });
