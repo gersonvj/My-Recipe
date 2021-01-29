736a0e4c4a76f811389af95d1dc5aab49a8336e2
gersonvj <gersonvj@hotmail.com>
2021-01-29 01:40:26 +0000

    User register/login functionality

--- app.py
+++ app.py
@@ -24,6 +24,29 @@ def get_ingredients():
     return render_template ("ingredients.html", ingredients=ingredients)
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+   
+
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
