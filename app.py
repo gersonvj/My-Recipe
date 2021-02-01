93a6b8edfdd6489df60849734d537af69bb6f9b1
gersonvj <gersonvj@hotmail.com>
2021-02-01 03:20:37 +0000

    edit submit to mongodb functinality

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def edit_recipe(recipe_id):
             "cooking": request.form.get("cooking"),
             "username": session["user"]
         }
-        mongo.db.ingredients.update({"_id": ObjectId(recipe_id)}, submit)
+        mongo.db.ingredients.update({"_id": ObjectId(recipe_id)}, recipe)
         flash("Task Successfully Updated")
 
     recipe = mongo.db.ingredients.find_one({"_id": ObjectId(recipe_id)})
