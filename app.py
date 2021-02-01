e8fe75f3c2e64b97bd57382bb1bf1127310b0765
gersonvj <gersonvj@hotmail.com>
2021-02-01 03:33:29 +0000

    logout function

--- app.py
+++ app.py
@@ -86,6 +86,14 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 @app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
     if request.method == "POST":
